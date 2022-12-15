eef0e0d5093fd90c8c529eaf999d03f9bbf010e3
Igor Felipe <83182736+pacotee@users.noreply.github.com>
2022-12-15 09:51:19 -0300

    change route api

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const docApi = require("../docs");
 const path = require("path");
 const erroValidacao = require("./middlewares/erroValidacao");
 const db = require("./database");
@@ -13,7 +14,8 @@ app.use(requestLog);
 app.use(routes);
 app.use(erroValidacao);
 
-app.use('./', express.static(path.resolve(__dirname, '..', 'docs',)));
+// app.use('/docs', express.static(path.resolve(__dirname, '..', 'docs',)));
+app.use("/", docApi);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
