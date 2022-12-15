9c563625414ee0d2b61006869980724414debd93
Igor Felipe <83182736+pacotee@users.noreply.github.com>
2022-12-15 11:41:28 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const docApi = require("../docs");
 const path = require("path");
 const erroValidacao = require("./middlewares/erroValidacao");
 const db = require("./database");
@@ -14,8 +13,7 @@ app.use(requestLog);
 app.use(routes);
 app.use(erroValidacao);
 
-// app.use('/docs', express.static(path.resolve(__dirname, '..', 'docs',)));
-// app.use("/", docApi);
+app.use('/docs', express.static(path.resolve(__dirname, '..', 'docs',)));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
